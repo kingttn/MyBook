2c0a721cbfef2de79708d5f30c8c41c349f93d65
phuoc hau <binpe09x@gmail.com>
2024-12-03 14:15:39 +0700

    Fix lai cac thu da push va pull

--- app/src/main/java/com/haunp/mybookstore/presenters/fragment/user/home/HomeViewModel.kt
+++ app/src/main/java/com/haunp/mybookstore/presenters/fragment/user/home/HomeViewModel.kt
@@ -2,13 +2,9 @@ package com.haunp.mybookstore.presenters.fragment.user.home
 
 import android.util.Log
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-<<<<<<< Updated upstream
-import androidx.lifecycle.asFlow
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.liveData
-import androidx.lifecycle.viewModelScope
 import com.haunp.mybookstore.domain.entity.BookEntity
 import com.haunp.mybookstore.domain.usecase.GetListBookUseCase
 import kotlinx.coroutines.launch
